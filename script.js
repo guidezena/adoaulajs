aab9ad302262dd033ea3c1c188acb7540fa760a5
guidezena <guilhermepereiradezena@gmail.com>
2022-08-25 11:36:01 -0300

    ado 1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const resultado = document.querySelector('#resultado')
-const altura = document.querySelector('#altura')
-const peso = document.querySelector('#peso')
+const resultado = document.querySelector('#resultado');
+const altura = document.querySelector('#altura');
+const peso = document.querySelector('#peso');
 
 const calcIMC = () => {
     if (altura.value !== '' && peso.value !== '') {
